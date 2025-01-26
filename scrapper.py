9d0f1b3be3ecae7564f7e30e5d1b6de505405071
kahmeeah <kao8144@nyu.edu>
2025-01-26 04:52:31 -0500

    update dependencies

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import sqlite3
 
 URL = "https://www.petpoisonhelpline.com/poisons/"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}

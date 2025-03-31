13b3e5d0df7e682fb28015114e5ec6158ad68557
kahmeeah <kao8144@nyu.edu>
2025-03-31 18:20:50 -0400

    feat: write function to insert toxins into table

--- database.py
+++ database.py
@@ -1,21 +1,38 @@
 import sqlite3
+import json
 
-# define connection and cursor 
+def insert_data(toxins):
+    # define connection and cursor 
 
-connection = sqlite3.connect('pet_toxins.db')
+    connection = sqlite3.connect('pet_toxins.db')
 
-cursor = connection.cursor()
+    cursor = connection.cursor()
 
-#create toxins table
+    #create toxins table
 
-createcommand = """CREATE TABLE IF NOT EXISTS
-toxins(toxin_id INTEGER PRIMARY KEY AUTOINCREMENT,
-name TEXT UNIQUE, category TEXT, link TEXT, image_url TEXT,
-alternate_names TEXT, description TEXT, symptoms TEXT,
-animals TEXT
-)"""
+    createcommand = """CREATE TABLE IF NOT EXISTS
+    toxins(toxin_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE, category TEXT, link TEXT, image_url TEXT,
+    alternate_names TEXT, description TEXT, symptoms TEXT,
+    animals TEXT
+    )"""
 
-cursor.execute(createcommand)
+    cursor.execute(createcommand)
 
-# import list of toxins from scrapper.py
-# insert each into table
+    # insert each toxin from toxins into table
+    for toxin in toxins:
+        cursor.execute("""INSERT INTO toxins (name, category, link, image_url, alternate_names, description, symptoms, animals
+                       VALUES (?, ?, ?, ?, ?, ?, ?, ?))
+                       """, (
+                           toxin["name"],
+                           toxin["category"],
+                           toxin["link"],
+                           toxin["image_url"],
+                           toxin["alternate_names"],
+                           toxin["description"],
+                           toxin["symptoms"],
+                           json.dumps(toxin["animals"])
+                       ))
+        
+    connection.commit()
+    connection.close()
